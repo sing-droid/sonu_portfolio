66cd69efa6c93b3239ff9f50697cf34b1b332e4f
SONU SINGH <sonusingh82342@gmail.com>
2023-05-26 07:27:46 +0530

    new1

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Heroimg2 from "../components/Heroimg2";
+import PricingCard from "../components/PricingCard";
+import Work from "../components/Work";
 
 const Project = ()=> {
     return (
 <div>
     <Navbar />
     <Heroimg2 heading = "Project" text="project on which i have worked."  />
+    <Work />
+    {/* <PricingCard /> */}
     <Footer />
 </div>
     );
