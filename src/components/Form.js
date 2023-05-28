f3345a3b2718fb74cc45ffdae0f1e75f39a2cfdf
SONU SINGH <sonusingh82342@gmail.com>
2023-05-29 01:58:31 +0530

    newcommit

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,20 @@ import "./FormStyle.css";
 
 const Form = () => {
     return (
-<div>
-    hello
+<div className="form">
+    <form>
+        <label>Your Name </label>
+        <input type="text" placeholder="Your Name"/>
+        <label>Email</label>
+        <input type="email" placeholder="Email-"/>
+        <label>Subject</label>
+        <input type="text" placeholder="Subject"/>
+        <label>Message</label>
+        <textarea rows="6" placeholder="Type your message here"></textarea>
+        <button className="btn">Submit</button>
+
+    </form>
+
 </div>
     );
 }
