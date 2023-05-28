479ed093df6ca49327e28653f8dc7850fc56d57a
SONU SINGH <sonusingh82342@gmail.com>
2023-05-29 02:02:25 +0530

    new

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,13 @@ const Form = () => {
 <div className="form">
     <form>
         <label>Your Name </label>
-        <input type="text" placeholder="Your Name"/>
+        <input type="text" placeholder="Your Name :-"/>
         <label>Email</label>
-        <input type="email" placeholder="Email-"/>
+        <input type="email" placeholder="Email :-"/>
         <label>Subject</label>
-        <input type="text" placeholder="Subject"/>
+        <input type="text" placeholder="Subject :-"/>
         <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here"></textarea>
+        <textarea rows="6" placeholder="Type your message here :-"></textarea>
         <button className="btn">Submit</button>
 
     </form>
